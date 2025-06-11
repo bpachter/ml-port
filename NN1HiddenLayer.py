f7937037a754dba393c1fa38aa9703253ec7b32a
Benjamin Pachter <ben.pachter@bellsouth.net>
2025-06-11 09:13:11 -0400

    fixed errors

--- NN1HiddenLayer.py
+++ NN1HiddenLayer.py
@@ -57,28 +57,28 @@ class Net(nn.Module):
         x = torch.sigmoid(self.linear1(x))
         x = self.linear2(x)
 
-    # Model training function
-    def train(model, criterion, train_loader, validation_loader, optimizer, epochs=100):
-        i = 0
-        useful_stuff = {'training_loss': [],'validation_accuracy': []}  
-        for epoch in range(epochs):
-            for i, (x, y) in enumerate(train_loader): 
-                optimizer.zero_grad()
-                z = model(x.view(-1, 28 * 28))
-                loss = criterion(z, y)
-                loss.backward()
-                optimizer.step()
-                # loss for every iteration
-                useful_stuff['training_loss'].append(loss.data.item())
-            correct = 0
-            for x, y in validation_loader:
-                # validation 
-                z = model(x.view(-1, 28 * 28))
-                _, label = torch.max(z, 1)
-                correct += (label == y).sum().item()
-            accuracy = 100 * (correct / len(validation_dataset))
-            useful_stuff['validation_accuracy'].append(accuracy)
-        return useful_stuff
+# Model training function
+def train(model, criterion, train_loader, validation_loader, optimizer, epochs=100):
+    i = 0
+    useful_stuff = {'training_loss': [],'validation_accuracy': []}  
+    for epoch in range(epochs):
+        for i, (x, y) in enumerate(train_loader): 
+            optimizer.zero_grad()
+            z = model(x.view(-1, 28 * 28))
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            # loss for every iteration
+            useful_stuff['training_loss'].append(loss.data.item())
+        correct = 0
+        for x, y in validation_loader:
+            # validation 
+            z = model(x.view(-1, 28 * 28))
+            _, label = torch.max(z, 1)
+            correct += (label == y).sum().item()
+        accuracy = 100 * (correct / len(validation_dataset))
+        useful_stuff['validation_accuracy'].append(accuracy)
+    return useful_stuff
 
 # create training dataset from MNIST
 train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
