a6b7f8535e195aa7b9d3a4bea5fa637b25c7454f
bpachter <ben.pachter@bellsouth.net>
2025-06-12 10:18:37 -0400

    updating to account for previous month stores

--- Shuttle1.vb
+++ Shuttle1.vb
@@ -1,21 +1,38 @@
 Public Function GetMissingStoresList() As Collection
     Dim wsGen As Worksheet
     Dim lastRow As Long, i As Long
+    Dim maxMonth As Long, maxYear As Long
     Dim store As String, serial As String, ca As String
+    Dim thisMonth As Long, thisYear As Long
     Dim missingStores As New Collection
 
     Set wsGen = ThisWorkbook.Sheets("Billing Interval Generator")
     lastRow = wsGen.Cells(wsGen.Rows.Count, "B").End(xlUp).Row
 
-    ' loop from row 4 down (header is in row 3)
+    ' step 1: find the latest month and year combination
     For i = 4 To lastRow
-        If Trim(wsGen.Cells(i, "H").Value) = "FALSE" Then
-            store = Trim(wsGen.Cells(i, "B").Value)
-            serial = Trim(wsGen.Cells(i, "G").Value)
-            ca = ""  ' contract account will be filled in manually
+        thisMonth = Val(wsGen.Cells(i, "C").Value)
+        thisYear = Val(wsGen.Cells(i, "D").Value)
+        If thisYear > maxYear Or (thisYear = maxYear And thisMonth > maxMonth) Then
+            maxMonth = thisMonth
+            maxYear = thisYear
+        End If
+    Next i
 
-            If Len(store) > 0 And Len(serial) > 0 Then
-                missingStores.Add Array(ca, serial, store)
+    ' step 2: collect missing stores only for latest month/year
+    For i = 4 To lastRow
+        thisMonth = Val(wsGen.Cells(i, "C").Value)
+        thisYear = Val(wsGen.Cells(i, "D").Value)
+
+        If thisMonth = maxMonth And thisYear = maxYear Then
+            If Trim(wsGen.Cells(i, "H").Value) = "FALSE" Then
+                store = Trim(wsGen.Cells(i, "B").Value)
+                serial = Trim(wsGen.Cells(i, "G").Value)
+                ca = "" ' contract account to be filled by user
+
+                If Len(store) > 0 And Len(serial) > 0 Then
+                    missingStores.Add Array(ca, serial, store)
+                End If
             End If
         End If
     Next i
