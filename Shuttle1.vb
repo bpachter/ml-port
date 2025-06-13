93b6655929239968cb6d537f1444c4d8407bf64a
bpachter <ben.pachter@bellsouth.net>
2025-06-13 07:24:21 -0400

    The third shuttle is away!

--- Shuttle1.vb
+++ Shuttle1.vb
@@ -1,29 +1,38 @@
-Private Sub UserForm_Initialize()
-    ' leave empty so nothing runs prematurely
-End Sub
+Private Sub cmbStoreNumber_Change()
+    If Not isInitialized Then Exit Sub
 
-Public Sub PopulateForm()
-    Set dictStoreData = CreateObject("Scripting.Dictionary")
-    
-    Dim sortedStores As New Collection
-    Dim i As Long, j As Long, inserted As Boolean
-    Dim store As Variant
+    ' save current store's values
+    If cmbStoreNumber.Tag <> "" Then
+        dictStoreData(cmbStoreNumber.Tag) = Array( _
+            txtContractAccount.Text, txtSerialNumber.Text, txtBillingStart.Text, _
+            txtBillingEnd.Text, txtBilledkWh.Text, txtBilledDemand.Text, _
+            txtLoadFactor.Text, txtDemandKVar.Text)
+    End If
 
-    ' sort pMissingStores by store number (element 3)
-    For i = 1 To pMissingStores.Count
-        inserted = False
-        For j = 1 To sortedStores.Count
-            If CLng(pMissingStores(i)(2)) < CLng(sortedStores(j)(2)) Then
-                sortedStores.Add pMissingStores(i), , j
-                inserted = True
-                Exit For
-            End If
-        Next j
-        If Not inserted Then sortedStores.Add pMissingStores(i)
-    Next i
+    ' clear inputs
+    txtContractAccount.Text = ""
+    txtSerialNumber.Text = ""
+    txtBillingStart.Text = ""
+    txtBillingEnd.Text = ""
+    txtBilledkWh.Text = ""
+    txtBilledDemand.Text = ""
+    txtLoadFactor.Text = ""
+    txtDemandKVar.Text = ""
 
-    cmbStoreNumber.Clear
-    For Each store In sortedStores
-        cmbStoreNumber.AddItem store(2)
-    Next store
+    ' load existing data if exists
+    If dictStoreData.exists(cmbStoreNumber.Value) Then
+        Dim values As Variant
+        values = dictStoreData(cmbStoreNumber.Value)
+
+        txtContractAccount.Text = values(0)
+        txtSerialNumber.Text = values(1)
+        txtBillingStart.Text = values(2)
+        txtBillingEnd.Text = values(3)
+        txtBilledkWh.Text = values(4)
+        txtBilledDemand.Text = values(5)
+        txtLoadFactor.Text = values(6)
+        txtDemandKVar.Text = values(7)
+    End If
+
+    cmbStoreNumber.Tag = cmbStoreNumber.Value
 End Sub
