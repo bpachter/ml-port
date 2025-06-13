9e8ca561807c4f1e2b0cf16779135afce9ea399f
bpachter <ben.pachter@bellsouth.net>
2025-06-13 12:01:24 -0400

    Adding Continue button

--- Shuttle1.vb
+++ Shuttle1.vb
@@ -21,3 +21,45 @@ For i = 2 To lastRow ' assuming row 1 has headers
         Debug.Print "[lookup initialized] store=" & storeVal & " CA=" & caVal & " SN=" & serialVal
     End If
 Next i
+
+Private Sub btnContinue_Click()
+    ' save current store's values before validating
+    If cmbStoreNumber.Tag <> "" Then
+        dictStoreData(cmbStoreNumber.Tag) = Array( _
+            txtContractAccount.Text, txtSerialNumber.Text, txtBillingStart.Text, _
+            txtBillingEnd.Text, txtBilledkWh.Text, txtBilledDemand.Text, _
+            txtLoadFactor.Text, txtDemandKVar.Text)
+    End If
+
+    ' validate all stores have data
+    Dim store As Variant
+    For Each store In cmbStoreNumber.List
+        If Not dictStoreData.exists(store) Then
+            MsgBox "Missing data for store " & store, vbExclamation
+            Exit Sub
+        End If
+
+        Dim vals As Variant: vals = dictStoreData(store)
+        Dim i As Long
+        For i = 0 To UBound(vals)
+            If Trim(vals(i)) = "" Then
+                MsgBox "Incomplete entry for store " & store, vbExclamation
+                Exit Sub
+            End If
+        Next i
+    Next store
+
+    ' write data to ASR - Bill Input
+    Dim ws As Worksheet: Set ws = ThisWorkbook.Sheets("ASR - Bill Input")
+    Dim startRow As Long: startRow = 6
+    ws.Range("D6:L10000").ClearContents
+
+    Dim r As Long: r = startRow
+    For Each store In cmbStoreNumber.List
+        vals = dictStoreData(store)
+        ws.Cells(r, "D").Resize(1, 9).Value = vals
+        r = r + 1
+    Next store
+
+    Me.Hide
+End Sub
